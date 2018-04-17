a489e01a9aa8a02fedd168ed17898cf0b82a8f18
John Crisanto <johnccrisanto@gmail.com>
2018-04-17 10:56:41 -0700

    Added test comment to test whether or not GitHub is updating accordingly upon committing and pushing.

--- src/main/java/com/johncrisanto/shoestore/ShoestoreApplication.java
+++ src/main/java/com/johncrisanto/shoestore/ShoestoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoestoreApplication {
 
 	public static void main(String[] args) {
+		// Test comment to double check GitHub updating. 
 		SpringApplication.run(ShoestoreApplication.class, args);
 	}
 }
