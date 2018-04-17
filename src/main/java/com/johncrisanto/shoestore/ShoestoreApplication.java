df63540136bc18ebbfc68c65ffd8c3821bc23f7b
John Crisanto <johnccrisanto@gmail.com>
2018-04-17 10:49:33 -0700

    Initial import to set up e-commerce project on GitHub.

---
+++ src/main/java/com/johncrisanto/shoestore/ShoestoreApplication.java
@@ -0,0 +1,12 @@
+package com.johncrisanto.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoestoreApplication.class, args);
+	}
+}
