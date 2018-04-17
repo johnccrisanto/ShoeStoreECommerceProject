f3c2b3986e25f70531fcaddab6adcb1fcaffedcf
John Crisanto <johnccrisanto@gmail.com>
2018-04-17 13:46:22 -0700

    Continue workin on index.html page. Edit navigation bar. Create a header template (header.html) that includes the common <head> links, navigation bar and javascript links. Referenced header template in index.html page.

--- src/main/java/com/johncrisanto/shoestore/ShoestoreApplication.java
+++ src/main/java/com/johncrisanto/shoestore/ShoestoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoestoreApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ShoestoreApplication.class, args);
 	}
 }
